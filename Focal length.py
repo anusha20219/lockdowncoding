f58ba5bcdf3eda526cb8bcdddf128fc7d4060ab7
anusha20219 <53473615+anusha20219@users.noreply.github.com>
2020-07-26 10:29:20 +0530

    Create Focal length.py

---
+++ Focal length.py
@@ -0,0 +1,14 @@
+def focal_length_concave(R): 
+    return R / 2
+  
+# Determines focal length of 
+# a spherical convex mirror 
+def focal_length_convex(R): 
+    return - ( R/ 2 )  
+  
+# Driver function 
+R = 30 ; 
+print("Focal length of spherical concave mirror is :", 
+        focal_length_concave(R)," units") 
+print("Focal length of spherical convex mirror is : ", 
+        focal_length_convex(R)," units") 
